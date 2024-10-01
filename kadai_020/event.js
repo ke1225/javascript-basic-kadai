18e66228c19b96e060fa5c23a3b2e721ad629b27
ke1225 <jsfwm210@gmail.com>
2024-10-01 14:22:08 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,5 +1,6 @@
 const btn=document.getElementById('btn');
+const text=document.getElementById('text');
 
 btn.addEventListener('click',()=>{
-  console.log('ボタンをクリックしました');
+  text.textContent='ボタンをクリックしました';
 });
